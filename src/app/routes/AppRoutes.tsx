b71a34325d830e70cdc065319c77cbf588266be2
Kris Osmakov <krisosmakov@MacBook-Air-Kris.local>
2025-06-23 21:26:53 +0300

    fix: fixed router

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -6,7 +6,7 @@ import BasePage from "../../pages/BasePage"
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<BasePage/>}/>
+            {/* <Route path="/" element={<BasePage/>}/> */}
             <Route path="/test" element={<SelectPage/>} />
             <Route path="/cards" element={<CardsPage />} />
         </Routes>
